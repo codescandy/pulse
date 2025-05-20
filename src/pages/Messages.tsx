375901ff0ed1773c451f64b3df790dae07872f7a
anitaparmar26 <easeanita@gmail.com>
2025-05-20 16:48:00 +0530

    setup repo

---
+++ src/pages/Messages.tsx
@@ -0,0 +1,16 @@
+import { DashboardLayout } from '@/components/layout/DashboardLayout';
+
+const Messages = () => {
+  return (
+    <DashboardLayout>
+      <div className="space-y-6">
+        <div>
+          <h1 className="text-2xl font-bold tracking-tight mb-2">Messages</h1>
+          <p className="text-muted-foreground">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, eos? Praesentium iusto unde consequuntur dolor aliquid!</p>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Messages;
